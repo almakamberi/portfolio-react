2f1f41ef3c887268cfe5043d694a7000181dc370
Gjin Prelvukaj <132224503+GjinPrelvukaj@users.noreply.github.com>
2023-05-03 00:24:48 +0200

    Update 5.0
    Added a 2.5s splash screen
    Added a Loading.js for the Splash Screen
    Added a box.svg for the splash screen

---
+++ src/Components/Loading.js
@@ -0,0 +1,45 @@
+import box from './box.svg'
+import React from 'react';
+
+function LoadingScreen() {
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+    style={{
+        backgroundColor: '#FFFFFF',
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        transition: 'opacity 1s ease-in-out', // add a transition
+        opacity: 1, // set the initial opacity to 1
+      }}
+    >
+      <img
+        src={box}
+        alt="My logo"
+        style={{
+          backgroundColor: "#FFF",
+          width: "50%",
+          height: "50%",
+          overflow: "hidden",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      />
+    </div>
+  );
+}
+
+export default LoadingScreen;
